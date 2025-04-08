af43d6d2f5a50ee5f0945f75ef5a4fd92fa58501
dard2002 <dylan.5software@gmail.com>
2025-04-08 15:55:25 +1000

    Added PracticeEvent/End table

--- init.sql
+++ init.sql
@@ -4,9 +4,30 @@ USE DATABASE ARCHERYDB;
 CREATE TABLE Member
 (
     ArcheryAustraliaID NVARCHAR( 255 ) NOT NULL,
-    FirstName NVARCHAR( 255 ),
-    DateOfBirth DATE,
-    Gender BOOL,
-    DefaultBowType NVARCHAR( 255 ),
+    FirstName NVARCHAR( 255 ) NOT NULL,
+    DateOfBirth DATE NOT NULL,
+    Gender BOOL NOT NULL,
+    DefaultBowType NVARCHAR( 255 ) NOT NULL,
     PRIMARY KEY ( ArcheryAustraliaID )
 );
+
+CREATE TABLE PracticeEvent
+(
+    PracticeID INT NOT NULL,
+    EventID INT NOT NULL,
+    ArcheryAustraliaID NVARCHAR( 255 ) NOT NULL,
+    PRIMARY KEY (PracticeID),
+    FOREIGN KEY (EventID) REFERENCES Event(EventID),
+    FOREIGN KEY (ArcheryAustraliaID) REFERENCES Member(ArcheryAustraliaID)
+);
+
+CREATE TABLE End
+(
+    EndID INT NOT NULL,
+    RangeID INT NOT NULL,
+    ArcheryAustraliaID NVARCHAR( 255 ) NOT NULL,
+    TargetNumber INT,
+    Score INT,
+    X INT
+);
+
