79c5b59694eb2c0f433d734324de7bf26b00c623
Luke McDonald <103056462@student.swin.edu.au>
2025-04-08 17:32:03 +1000

    add initial working draft

--- init.sql
+++ init.sql
@@ -1,56 +1,65 @@
 CREATE DATABASE IF NOT EXISTS ARCHERYDB;
 USE ARCHERYDB;
 
+CREATE TABLE IF NOT EXISTS Class (
+    ClassID NVARCHAR(255) PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS Division (
+    DivisionID NVARCHAR(255) PRIMARY KEY
+);
+
 CREATE TABLE IF NOT EXISTS Member
 (
-    ArcheryAustraliaID NVARCHAR( 255 ) PRIMARY KEY NOT NULL,
-    FirstName NVARCHAR( 255 ),
+    ArcheryAustraliaID NVARCHAR(255) PRIMARY KEY NOT NULL,
+    FirstName NVARCHAR(255),
     DateOfBirth DATE,
     Gender BOOL,
-    DefaultBowType NVARCHAR( 255 ),
+    DefaultDivision NVARCHAR(255),
+    FOREIGN KEY (DefaultDivision) REFERENCES Division(DivisionID)
 );
 
-CREATE TABLE Event (
+CREATE TABLE IF NOT EXISTS Event (
     EventID INT AUTO_INCREMENT PRIMARY KEY,
-    Name VARCHAR(255) NOT NULL,
-    Date DATE NOT NULL,
+    Name NVARCHAR(255) NOT NULL,
+    Date DATE NOT NULL
 );
 
-CREATE TABLE Championship (
+CREATE TABLE IF NOT EXISTS Championship (
     ChampionshipID INT PRIMARY KEY NOT NULL ,  
     EventID INT NOT NULL,
-    Name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (EventID) 
-        REFERENCES Event(EventID)
+    Name NVARCHAR(255) NOT NULL,
+    FOREIGN KEY (EventID) REFERENCES Event(EventID)
  );
  
-CREATE TABLE Round (
+CREATE TABLE IF NOT EXISTS Round (
     RoundID INT AUTO_INCREMENT PRIMARY KEY,
     EventID INT NOT NULL,
-    Division VARCHAR(255) NOT NULL,
-    Class VARCHAR(255) NOT NULL,
+    Division NVARCHAR(255) NOT NULL,
+    Class NVARCHAR(255) NOT NULL,
     Gender BOOL,
     FOREIGN KEY (EventID) REFERENCES Event(EventID),
     FOREIGN KEY (Division) REFERENCES Division(DivisionID),
     FOREIGN KEY (Class) REFERENCES Class(ClassID)
 );
-  
-CREATE TABLE Range (
+
+CREATE TABLE IF NOT EXISTS `Range` 
+(
     RangeID INT AUTO_INCREMENT PRIMARY KEY,
     RoundID INT NOT NULL,
     Staged BOOL NOT NULL,
     Distance INT NOT NULL,
     TargetSize INT NOT NULL,
-    FOREIGN KEY (RoundID) REFERENCES Round(RoundID)
+    FOREIGN KEY (RoundID) REFERENCES `Round`(RoundID)
 );
 
 CREATE TABLE IF NOT EXISTS PracticeEvent
 (
     PracticeID INT AUTO_INCREMENT PRIMARY KEY,
     EventID INT NOT NULL,
-    ArcheryAustraliaID NVARCHAR( 255 ) NOT NULL,
-    FOREIGN KEY (EventID) REFERENCES Event(EventID),
-    FOREIGN KEY (ArcheryAustraliaID) REFERENCES Member(ArcheryAustraliaID)
+    ArcheryAustraliaID NVARCHAR(255) NOT NULL,
+    FOREIGN KEY (ArcheryAustraliaID) REFERENCES Member(ArcheryAustraliaID),
+    FOREIGN KEY (EventID) REFERENCES Event(EventID)
 );
 
 CREATE TABLE IF NOT EXISTS End
@@ -58,8 +67,8 @@ CREATE TABLE IF NOT EXISTS End
     EndID INT AUTO_INCREMENT PRIMARY KEY,
     RangeID INT NOT NULL,
     ArcheryAustraliaID NVARCHAR( 255 ) NOT NULL,
-    FinalScore INT,
-    FOREIGN KEY (RangeID) REFERENCES Range(RangeID),
+    FinalScore INT NOT NULL,
+    FOREIGN KEY (RangeID) REFERENCES `Range`(RangeID),
     FOREIGN KEY (ArcheryAustraliaID) REFERENCES Member(ArcheryAustraliaID)
 );
 
@@ -67,12 +76,7 @@ CREATE TABLE IF NOT EXISTS Score
 (
     ScoreID INT AUTO_INCREMENT PRIMARY KEY,
     EndID INT NOT NULL,
-    ArrowNumber INT,
-    Score INT,
+    ArrowNumber INT NOT NULL,
+    Score INT NOT NULL,
     FOREIGN KEY (EndID) REFERENCES End(EndID)
-);
-
-CREATE TABLE IF NOT EXISTS Class (
-    ClassID INT AUTO_INCREMENT PRIMARY KEY,
-    PRIMARY KEY ( ClassID )
 );
