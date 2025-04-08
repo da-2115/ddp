793dbe8f5228d4ec8e9af612bbea00711caa20d0
sinning572 <sinning572@gmail.com>
2025-04-08 16:04:08 +1000

    Add championship and event

--- init.sql
+++ init.sql
@@ -10,3 +10,20 @@ CREATE TABLE Member
     DefaultBowType NVARCHAR( 255 ),
     PRIMARY KEY ( ArcheryAustraliaID )
 );
+
+CREATE TABLE Event (
+    EventID INT NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    Date DATE NOT NULL,
+    PRIMARY KEY (EventID)  
+);
+
+CREATE TABLE Championship (
+    ChampionshipID INT NOT NULL,  
+    EventID INT NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (ChampionshipID),  
+    CONSTRAINT FK_Championship_Event 
+    FOREIGN KEY (EventID) 
+        REFERENCES Event(EventID)
+);
