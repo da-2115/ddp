87af854a5363d0a46cab4e286d25e2428c8d4429
dard2002 <dylan.5software@gmail.com>
2025-04-08 16:08:31 +1000

    Added PracticeEvent/End table + now with score table

--- init.sql
+++ init.sql
@@ -13,13 +13,22 @@ CREATE TABLE Member
 
 CREATE TABLE Round (
     RoundID INT PRIMARY KEY,
-    EventID INT,
-    Division INT,
-    Class INT,
+    EventID INT NOT NULL,
+    Division VARCHAR(255) NOT NULL,
+    Class VARCHAR(255) NOT NULL,
     Gender BOOL,
     FOREIGN KEY (EventID) REFERENCES Event(EventID),
     FOREIGN KEY (Division) REFERENCES Division(DivisionID),
     FOREIGN KEY (Class) REFERENCES Class(ClassID)
+);
+
+CREATE TABLE Range (
+    RangeID INT PRIMARY KEY,
+    RoundID INT NOT NULL,
+    Staged BOOL NOT NULL,
+    Distance INT NOT NULL,
+    TargetSize INT NOT NULL,
+    FOREIGN KEY (RoundID) REFERENCES Round(RoundID)
 );
 CREATE TABLE IF NOT EXISTS PracticeEvent
 (
