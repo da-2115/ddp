c9eaed32eac94bd5f30f47fd4edeba769e70f572
shrimp332 <101687942+shrimp332@users.noreply.github.com>
2025-04-27 22:54:40 +1000

    add password + club recorder

--- init.sql
+++ init.sql
@@ -31,9 +31,11 @@ VALUES
 CREATE TABLE IF NOT EXISTS Member
 (
     ArcheryAustraliaID NVARCHAR(255) PRIMARY KEY NOT NULL,
+    PasswordHash VARCHAR(255) NOT NULL,
     FirstName NVARCHAR(255) NOT NULL,
     DateOfBirth DATE NOT NULL,
     Gender BOOL NOT NULL,
+    ClubRecorder BOOL NOT NULL,
     DefaultDivision NVARCHAR(255) NOT NULL,
     FOREIGN KEY (DefaultDivision) REFERENCES Division(DivisionID)
 );
