52cd9d20891a94ae478b8aa334887c9c6801091b
sinning572 <sinning572@gmail.com>
2025-05-19 20:59:02 +1000

    Update setup-security.sql

--- setup-security.sql
+++ setup-security.sql
@@ -1,11 +1,16 @@
--- setup-security.sql
--- Create limited database users for web access
-
--- Create a limited-access user for the web application
+-- Create limited-access user for web application
 CREATE USER 'webapp_user'@'%' IDENTIFIED BY 'WebAppDB2025!';
 
--- Grant only necessary permissions to specific tables
-GRANT SELECT, INSERT, UPDATE ON ARCHERYDB.Member TO 'webapp_user'@'%';
-GRANT SELECT ON ARCHERYDB.Event TO 'webapp_user'@'%';
-GRANT SELECT, INSERT ON ARCHERYDB.End TO 'webapp_user'@'%';
-GRANT SELECT, INSERT ON ARCHERYDB.Score TO 'webapp_user'@'%';
+-- Select, Insert
+GRANT SELECT, INSERT ON ARCHERYDB.Event TO 'webapp_user'@'%';
+GRANT SELECT, INSERT ON ARCHERYDB.Round TO 'webapp_user'@'%';
+GRANT SELECT, INSERT ON ARCHERYDB.Range TO 'webapp_user'@'%';
+GRANT SELECT, INSERT ON ARCHERYDB.PracticeEvent TO 'webapp_user'@'%';
+GRANT SELECT, INSERT ON ARCHERYDB.Championship TO 'webapp_user'@'%';
+
+-- Select, Insert, Update, Delete
+GRANT SELECT, INSERT, UPDATE, DELETE ON ARCHERYDB.End TO 'webapp_user'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ARCHERYDB.Score TO 'webapp_user'@'%';
+
+-- Select only
+GRANT SELECT ON ARCHERYDB.Member TO 'webapp_user'@'%';
